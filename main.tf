698e4b383f0108cda3817f6b9dc96f9d4a0e3a63
PravinMakula <Pravindevops5@gmail.com>
2024-02-15 23:28:07 +0530

    Commit2

--- main.tf
+++ main.tf
@@ -6,4 +6,8 @@ resource "aws_instance" "name" {
   ami = "ami-03f4878755434977f"
   instance_type = "t2.micro"
   key_name = "Terraform_Key"
+  tags = {
+    Name = "my-ec2-instance"
+    "Terraform" = "Yes"
+    }
 }
